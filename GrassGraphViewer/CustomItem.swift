36f282e0d59c1dc928e2b0d8202b91bd161cd58a
Enchan <enchant0130@gmail.com>
2021-02-01 16:21:17 +0900

    [Update] show grass color in each cell

--- GrassGraphViewer/CustomItem.swift
+++ GrassGraphViewer/CustomItem.swift
@@ -8,17 +8,43 @@
 import Cocoa
 
 class CustomItem: NSCollectionViewItem {
+    
+    var color: NSColor!
+    var backgroundTileView: OpaqueView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear() {
+        // 背景色を設定するための子ビューを追加
+        backgroundTileView = OpaqueView(frame: .zero, color: color)
+        view.addSubview(backgroundTileView)
+    }
+    
+    override func viewDidLayout() {
+        // frameを更新
+        let tileViewFrame = NSRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        backgroundTileView?.frame = tileViewFrame
+    }
+    
 }
 
-class CustomView: NSView{
+class OpaqueView: NSView{
+    private var backgroundColor: NSColor!
+    
+    init(frame frameRect: NSRect, color: NSColor) {
+        super.init(frame: frameRect)
+        self.backgroundColor = color
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
-        NSColor.gray.setFill()
+        self.backgroundColor.setFill()
         dirtyRect.fill()
     }
 }
--- GrassGraphViewer/ViewController.swift
+++ GrassGraphViewer/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: NSViewController {
     @IBOutlet weak var collectionView: NSCollectionView!
     @IBOutlet weak var flowLayout: NSCollectionViewFlowLayout!
     
+    var contributions: [Int] = []
+    var currentMaxContribution = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,30 +24,54 @@ class ViewController: NSViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
 
-        flowLayout.minimumLineSpacing = 10
-        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.sectionInset = .init(top: 10, left: 10, bottom: 10, right: 10)
+        flowLayout.minimumLineSpacing = 2
+        flowLayout.minimumInteritemSpacing = 2
+        flowLayout.scrollDirection = .horizontal
+        
+        // ダミーデータを突っ込む ここクソ実装
+        for _ in 0..<365{
+            // 一定確率で穴を開けてそれっぽくする
+            let data: Int
+            if(Int.random(in: 0..<3) == 0){
+                data = 0
+            }else{
+                data = Int.random(in: 0..<5)
+            }
+            
+            contributions.append(data)
+        }
+        currentMaxContribution = contributions.max() ?? 0
         
         collectionView.reloadData()
-        
-    }
-    
-    override func viewDidAppear() {
-        
     }
     
     override func viewDidLayout() {
-        // collectionViewのフレームを取得
-        let collectionViewFrame = collectionView.frame
-        print(collectionViewFrame)
+        updateCellSize()
+    }
+    
+    // セルサイズ更新
+    func updateCellSize(){
+        // 親ビューのフレームを取得
+        let collectionViewFrame = self.view.frame
         
-        // TODO: なんかここカクつく collectionView.frameから計算してるせいでなんかおかしい説ある
-        // アイテム幅を計算
+        // collectionviewのマージンを取得
+        let collectionViewInsets = (collectionView.collectionViewLayout as? NSCollectionViewFlowLayout)?.sectionInset
+        
+        // 横軸、縦軸に置きたいアイテム数を設定
         let numberOfItemsInColumn: CGFloat = 5
-        let numberOfItemsInRow: CGFloat = 3
-        let itemWidth = (collectionViewFrame.width - flowLayout.minimumLineSpacing * (numberOfItemsInColumn - 1)) / numberOfItemsInColumn
-        let itemHeight = (collectionViewFrame.height - flowLayout.minimumInteritemSpacing * (numberOfItemsInRow - 1)) / numberOfItemsInRow
+        let numberOfItemsInRow: CGFloat = 7
         
-        flowLayout.itemSize = NSSize(width: itemWidth, height: itemHeight)
+        // アイテム幅を計算
+        let widthSpacings = flowLayout.minimumLineSpacing * (numberOfItemsInColumn - 1)
+        let heightSpacings = flowLayout.minimumInteritemSpacing * (numberOfItemsInRow - 1)
+        let verticalInsets = (collectionViewInsets?.top ?? 0) + (collectionViewInsets?.bottom ?? 0)
+        let horizontalInsets = (collectionViewInsets?.left ?? 0) + (collectionViewInsets?.right ?? 0)
+        
+        let itemWidth = (collectionViewFrame.width - widthSpacings - horizontalInsets) / numberOfItemsInColumn
+        let itemHeight = (collectionViewFrame.height - heightSpacings - verticalInsets) / numberOfItemsInRow
+        
+        flowLayout.itemSize = NSSize(width: itemHeight, height: itemHeight)
     }
 
     override var representedObject: Any? {
@@ -61,12 +88,28 @@ extension ViewController: NSCollectionViewDelegate {
 }
 
 extension ViewController: NSCollectionViewDataSource {
+    
     func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 19
+        let columns = 53
+        return columns * 7
     }
     
     func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        
         let item = collectionView.makeItem(withIdentifier: .init("item"), for: indexPath) as! CustomItem
+        
+        // 生える草の色を設定
+        if(indexPath[1] < self.contributions.count && self.contributions[indexPath[1]] > 0){
+            // 0.0~1.0 コミットするほど0に近づく
+            let contributionRate = 1.0 - Double(self.contributions[indexPath[1]]) / Double(self.currentMaxContribution)
+            
+            // 0.3~0.7に変換
+            let brightness = contributionRate * 0.4 + 0.3
+            
+            item.color = .init(hue: 0.33, saturation: 0.8, brightness: CGFloat(brightness), alpha: 1)
+        }else{
+            item.color = .lightGray
+        }
         return item
     }
     
