dde54f9df4d53aece7d64b0c7859b6d80a2c9cef
Enchan <enchant0130@gmail.com>
2021-02-01 11:25:06 +0900

    Initial Commit

---
+++ GrassGraphViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GrassGraphViewer
+//
+//  Created by EnchantCode on 2021/02/01.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GrassGraphViewer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GrassGraphViewer
+//
+//  Created by EnchantCode on 2021/02/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
