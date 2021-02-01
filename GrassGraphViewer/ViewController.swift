b4702beb5fb90571bbe1c1c116bf7c1ff488d1b6
Enchan <enchant0130@gmail.com>
2021-02-01 13:34:01 +0900

    [Update] set window transparent

--- GrassGraphViewer/ViewController.swift
+++ GrassGraphViewer/ViewController.swift
@@ -6,21 +6,92 @@
 //
 
 import Cocoa
+import CoreImage
+import CoreGraphics
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+        
+        // 画像ビュー配置
+        let imageURLString = "https://enchan-lab.net/Storage/icon.jpg"
+        guard let image = NSImage(contentsOf: URL(string: imageURLString)!) else{
+            fatalError("Can't load image from file!")
         }
+        let imageView = CustomImageView(image: image)
+        imageView.frame = NSRect(x: 0, y: 0, width: 400, height: 300)
+        imageView.wantsLayer = true
+        imageView.layer?.backgroundColor = .black
+        self.view.addSubview(imageView)
+    }
+    
+    override func viewWillAppear() {
+        // ウィンドウ初期化
+        if let window = self.view.window{
+            initWindowAppearance(window: window)
+        }else{
+            assertionFailure("Window object is nil!")
+        }
+        
+        // ウィンドウ枠を設定
+        self.view.wantsLayer = true
+        self.view.layer?.cornerRadius = 10
+        self.view.layer?.borderWidth = 5
+        self.view.layer?.borderColor = .init(gray: 0.2, alpha: 1)
+    }
+    
+    override func viewDidAppear() {
     }
 
-
+    // representedObject
+    
+    // ウィンドウ初期化
+    func initWindowAppearance(window: NSWindow){
+        // 背景の透明化
+        window.backgroundColor = .init(white: 1, alpha: 0)
+        window.isOpaque = false
+        
+        // ウィンドウ移動設定
+        // window.isMovable = false // ウィンドウを動かさない
+        window.isMovableByWindowBackground = true
+        
+        // その他諸々を隠す
+        window.titlebarAppearsTransparent = true
+        window.title = "GrassGraphViewer"
+        window.titleVisibility = .hidden
+        window.styleMask.remove(.titled)
+        window.styleMask.remove(.closable)
+        
+        // ウィンドウを配置する位置を設定
+        // desktopIconWindow: デスクトップとアイコンの間、ドラッグ不能
+        // normalWindow: 通常アプリケーションと同様の挙動
+        let windowLevel = CGWindowLevelForKey(.normalWindow)
+        window.level = NSWindow.Level(rawValue: Int(windowLevel))
+        
+        // window.ignoresMouseEvents = true // マウスイベントを無視
+        
+    }
+    
 }
 
+class CustomView: NSView {
+    // y軸を入れ替える
+    override var isFlipped: Bool{
+        get{
+            return true;
+        }
+    }
+    // CGで描画する場合は必須
+    // CGContextTranslateCTM(layerContext, 0, layerHeight );
+    // CGContextScaleCTM(layerContext, 1, -1);
+}
+
+class CustomImageView: NSImageView {
+    // マウスクリックで移動できるように
+    override var mouseDownCanMoveWindow: Bool{
+        get{
+            return true
+        }
+    }
+}
